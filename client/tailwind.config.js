9a3806eb9eeabd8438eee15f790450ad8150b662
Amankumar-033 <amanbkp9135@gmail.com>
2025-07-19 00:03:42 +0530

    Initializing git repo , installed all dependency

---
+++ client/tailwind.config.js
@@ -0,0 +1,27 @@
+// tailwind.config.js
+
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './app/**/*.{js,ts,jsx,tsx,mdx}',      // Scans files in the 'app' directory (for App Router pages/layouts)
+    './pages/**/*.{js,ts,jsx,tsx,mdx}',    // Scans files in the 'pages' directory (if you still have Pages Router pages)
+    './components/**/*.{js,ts,jsx,tsx,mdx}', // Scans files in your 'components' directory
+    // Add any other directories where you create files that use Tailwind classes
+    // e.g., './public/**/*.html', if you have static HTML files with Tailwind classes
+  ],
+  theme: {
+    extend: {
+      colors: {
+        lightHover: '#fcf4ff',
+        darkHover: '#2a004a',
+        darkTheme: '#11001f',
+      },
+      fontFamily: {
+        Outfit: ["Outfit", "sans-serif"],
+        Ovo: ["Ovo", "serif"],
+      }
+    },
+  },
+  plugins: [],
+}
