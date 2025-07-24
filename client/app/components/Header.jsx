4887c34b1fadd4ed2a49d9bf6b3c51f938829cc9
Amankumar-033 <amanbkp9135@gmail.com>
2025-07-24 17:33:59 +0530

    auto-commit

--- client/app/components/Header.jsx
+++ client/app/components/Header.jsx
@@ -9,20 +9,20 @@ const Header = () => {
   return (
     <div className='w-11/12 max-w-3xl mx-auto text-center h-screen flex flex-col items-center justify-center gap-4 '>
 
-      <motion.div initial={{scale: 0}} whileInView={{scale: 1}} transition={{duration: 0.8, type:'spring' , stiffness: 100}}>
+      <motion.div initial={{scale: 0}} whileInView={{scale: 1}} transition={{duration: 0.8, type:'spring', stiffness: 100}}>
         {/* UserName profile picture */}
         <Image src={assets.profile_img} alt='profile-image' className='rounded-full w-32'/>
       </motion.div>
 
         {/* UserName description */}
-        <motion.h3 initial={{y: -20, opacity: 0}} whileInView={{y: 0, opacity: 1}} transition={{duration: 0.6, delay: 0.3}} className='flex items-end gap-2 text-xl md:text-2xl mb-3 font-Ovo'> Hi! I am Aman kumar <Image src={assets.hand_icon} alt='profile-hand-icon' className='w-6'/></,h3>
-        <motion.h1 initial={{y: -30, opacity: 0}} whileInView={{y: 0, opacity: 1}} transition={{duration: 0.8, delay: 0.5} className='text-3xl sm:text-6xl lg:text-[66px] font-Ovo'> Full stack web developer based in Bihar. </motion.h1>
-        <p className='max w-2xl mx-auto font-Ovo'> I am a full stack developer from Bihar, India with 2 years of experience in not multiple companies like TCS, Capgemini, Hyundai </p>
+        <motion.h3 initial={{y: -20, opacity: 0}} whileInView={{y: 0, opacity: 1}} transition={{duration: 0.6, delay: 0.3}} className='flex items-end gap-2 text-xl md:text-2xl mb-3 font-Ovo'> Hi! I am Aman kumar <Image src={assets.hand_icon} alt='profile-hand-icon' className='w-6'/></motion.h3>
+        <motion.h1 initial={{y: -30, opacity: 0}} whileInView={{y: 0, opacity: 1}} transition={{duration: 0.8, delay: 0.5}} className='text-3xl sm:text-6xl lg:text-[66px] font-Ovo'> Full stack web developer based in Bihar. </motion.h1>
+        <motion.p  initial={{opacity: 0}} whileInView={{opacity: 1}} transition={{duration: 0.6, delay: 0.7}} className='max w-2xl mx-auto font-Ovo'> I am a full stack developer from Bihar, India with 2 years of experience in not multiple companies like TCS, Capgemini, Hyundai </motion.p>
         
         {/* Contact and resume option */}
         <div className='flex flex-col sm:flex-row items-center gap-4 mt-4'>
-            <a href='#contact' className='px-10 py-3 border border-white rounded-full bg-black text-white flex items-center gap-2 dark:bg-transparent'>contact me <Image src={assets.right_arrow_white} alt='' className='w-4'/> </a>
-            <a href='/sample-resume.pdf' download className='px-10 py-3 border rounded-full border-gray-500 flex items-center gap-2 bg-white dark:text-black'>my resume <Image src={assets.download_icon} alt='' className='w-4'/> </a>
+            <motion.a initial={{y: 30, opacity: 0}} whileInView={{y: 0, opacity: 1}} transition={{duration: 0.6, delay: 1}} href='#contact' className='px-10 py-3 border border-white rounded-full bg-black text-white flex items-center gap-2 dark:bg-transparent'>contact me <Image src={assets.right_arrow_white} alt='' className='w-4'/> </motion.a>
+            <motion.a initial={{y: 30, opacity: 0}} whileInView={{y: 0, opacity: 1}} transition={{duration: 0.6, delay: 1.2}} href='/sample-resume.pdf' download className='px-10 py-3 border rounded-full border-gray-500 flex items-center gap-2 bg-white dark:text-black'>my resume <Image src={assets.download_icon} alt='' className='w-4'/> </motion.a>
         </div>
     </div>
   )
